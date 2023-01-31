cad8104e15295e85c53aabc334ff1dd205b99fd0
babali1996 <ulfetp@bk.ru>
2023-01-31 15:48:40 +0400

    ul none when list is empty, sort icon change, dont append when input value is empty

--- app.js
+++ app.js
@@ -4,39 +4,40 @@ const input = document.querySelector("input");
 const deleteText = document.querySelector(".delete");
 const addName = document.querySelector(".addName");
 const ul = document.querySelector(".list");
+const up = document.querySelector(".up")
+const down = document.querySelector(".down")
 
 addName.addEventListener("click", (e) => {
   e.preventDefault();
   let li = document.createElement("li");
   let spn = document.createElement('span')
-  // li.innerText = input.value;
   let btn = document.createElement("button");
   let img = document.createElement("img");
   btn.classList.add("remove");
   img.src = "./icons/Group 77.svg";
   img.setAttribute("alt", "");
-  btn.append(img);
-  li.append(btn)
-  spn.innerText = input.value
-  li.append(spn)
-  ul.append(li);
-  if ((ul.style.display = "none")) {
-    ul.style.display = "block";
+  if (input.value !== "") {
+    btn.append(img);
+    li.append(btn)
+    spn.innerText = input.value
+    li.append(spn)
+    ul.append(li);
+    ul.style.display = "block"
+  } else {
+    ul.style.display = "none"
+    
   }
-  else if (ul.firstChild.value == "") {
-    ul.style.display = "none";
-  }
-  else { }
+
 
   btn.addEventListener("click", (e) => {
     e.target.parentElement.parentElement.remove();
-    if (ul.firstChild.value == "") {
-      ul.style.display = "none";
-    } else { }
+    if (ul.innerText == "") {
+      ul.style.display = "none"
+    }
   });
 
-
 });
+
 
 deleteText.addEventListener("click", (e) => {
   input.value = "";
@@ -50,14 +51,19 @@ iconSort.addEventListener("click", () => {
   listElement.forEach((item) => {
     arr.push(item.textContent)
   })
-  
+
   if (sorted) {
+    up.style.display = "none"
+    down.style.display = "block"
     arr.sort()
+
     sorted = false
     listElement.forEach((item, index) => {
       item.textContent = arr[index]
     })
   } else {
+    up.style.display = "block"
+    down.style.display = "none"
     arr.reverse()
     listElement.forEach((item, index) => {
       item.textContent = arr[index]
